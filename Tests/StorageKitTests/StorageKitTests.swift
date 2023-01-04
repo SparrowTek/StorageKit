57de7cc7feda2c20a06aebd37309e661ce19393a
Thomas Rademaker <thomas@sparrowtek.com>
2023-01-04 17:27:02 -0500

    initial commit

---
+++ Tests/StorageKitTests/StorageKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import StorageKit
+
+final class StorageKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(StorageKit().text, "Hello, World!")
+    }
+}
